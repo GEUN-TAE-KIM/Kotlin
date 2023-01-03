96235d8cd37fc5d5cdaf07cc6c03a3853ee3110e
GEUN-TAE-KIM <rmsxo5678@naver.com>
2023-01-03 16:42:44 +0900

    함수와 기본 인수

--- app/src/main/java/org/cream/kotlin/day/day.kt
+++ app/src/main/java/org/cream/kotlin/day/day.kt
@@ -1,12 +1,50 @@
 package org.cream.kotlin.day
 
+import java.util.*
+
 /*
 TODO
  변수
  */
-fun main() {
-    var first = 1
-    first = 2
-    val two = 2
-    println(first + two)
+fun main1() {
+
+    val scan = Scanner(System.`in`)
+    val a = scan.nextDouble()
+    val b = scan.nextDouble()
+    println(a / b)
+
 }
+
+/*
+TODO
+ 함수
+ */
+fun main2() {
+    val result = plus(7, 3)
+    println(result)
+}
+
+fun plus(a: Int, b: Int): Int {
+    return a + b
+}
+
+/*
+TODO
+ 기본 인수
+ */
+fun minus(a: Int = 5, b: Int = 2) = a - b
+
+fun main() {
+    val result = minus(a = 10)
+    println(result)
+}
+
+
+
+
+
+
+
+
+
+
